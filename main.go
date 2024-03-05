a8fbf900fd2caf912644fc27f09ffab9308a7f8a
yunpengfei <chwyunpengfei@163.com>
2024-03-05 23:23:01 +0800

    配置方案的实现

--- main.go
+++ main.go
@@ -1,22 +1,38 @@
-/*
-* @Author: yun
-* @Date:   2024/3/5 22:40
- */
 package main
 
 import (
+	"flag"
 	"fmt"
-	"github.com/gin-gonic/gin"
 	"gohub/bootstrap"
+	btsConfig "gohub/config"
+	"gohub/pkg/config"
+
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	r := gin.New()
-	// TODO: 路由初始化
-	bootstrap.SetupRoute(r)
+func init() {
+	// 加载 config 目录下的配置信息
+	btsConfig.Initialize()
+}
 
-	err := r.Run(":3000")
+func main() {
+
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
+
+	// new 一个 Gin Engine 实例
+	router := gin.New()
+
+	// 初始化路由绑定
+	bootstrap.SetupRoute(router)
+
+	// 运行服务
+	err := router.Run(":" + config.Get("app.port"))
 	if err != nil {
+		// 错误处理，端口被占用了或者其他错误
 		fmt.Println(err.Error())
 	}
 }
