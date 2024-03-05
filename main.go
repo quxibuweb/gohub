61a133d45814df0f35ed9ccf78f73324a2797d87
yunpengfei <chwyunpengfei@163.com>
2024-03-05 22:57:35 +0800

    集成gin

--- main.go
+++ main.go
@@ -4,8 +4,33 @@
  */
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strings"
+)
 
 func main() {
-	fmt.Println("Hello Go")
+	r := gin.New()
+	r.GET("/", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"Hello": "Go",
+		})
+	})
+	r.NoRoute(func(ctx *gin.Context) {
+		acceptString := ctx.Request.Header.Get("Accept")
+		if strings.Contains(acceptString, "text/html") {
+			ctx.String(http.StatusOK, "无数据")
+		} else {
+			ctx.JSON(http.StatusOK, gin.H{
+				"error_no":      404,
+				"error_message": "暂无数据",
+			})
+		}
+	})
+	err := r.Run(":3000")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 }
