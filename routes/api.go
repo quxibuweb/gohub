3e3209a37d0191e503399fb819c926dc7af09926
yunpengfei <chwyunpengfei@163.com>
2024-03-05 23:34:06 +0800

    手机是否注册接口

--- routes/api.go
+++ routes/api.go
@@ -1,23 +1,23 @@
-// Package routes /*
-/*
-* @Author: yun
-* @Date:   2024/3/5 23:04
-* 路由文件 注册路由
- */
+// Package routes 注册路由
 package routes
 
 import (
+	"gohub/app/http/controllers/api/v1/auth"
+
 	"github.com/gin-gonic/gin"
-	"net/http"
 )
 
-func RegisterAPIRoutes(router *gin.Engine) {
-	v1 := router.Group("/v1")
+// RegisterAPIRoutes 注册网页相关路由
+func RegisterAPIRoutes(r *gin.Engine) {
+
+	// 测试一个 v1 的路由组，我们所有的 v1 版本的路由都将存放到这里
+	v1 := r.Group("/v1")
 	{
-		v1.GET("/", func(c *gin.Context) {
-			c.JSON(http.StatusOK, gin.H{
-				"Hello": "Go..",
-			})
-		})
+		authGroup := v1.Group("/auth")
+		{
+			suc := new(auth.SignupController)
+			// 判断手机是否已注册
+			authGroup.POST("/signup/phone/exist", suc.IsPhoneExist)
+		}
 	}
 }
