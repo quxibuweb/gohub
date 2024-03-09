c9d5591310850aa3052a1b3d9991dbfdbef9bef7
yunpengfei <chwyunpengfei@163.com>
2024-03-09 19:37:10 +0800

    用户列表

--- app/http/controllers/api/v1/users_controller.go
+++ app/http/controllers/api/v1/users_controller.go
@@ -1,10 +1,10 @@
 package v1
 
 import (
+	"github.com/gin-gonic/gin"
+	"gohub/app/models/user"
 	"gohub/pkg/auth"
 	"gohub/pkg/response"
-
-	"github.com/gin-gonic/gin"
 )
 
 type UsersController struct {
@@ -15,4 +15,10 @@ type UsersController struct {
 func (ctrl *UsersController) CurrentUser(c *gin.Context) {
 	userModel := auth.CurrentUser(c)
 	response.Data(c, userModel)
+}
+
+// Index 所有用户
+func (ctrl *UsersController) Index(c *gin.Context) {
+	data := user.All()
+	response.Data(c, data)
 }
